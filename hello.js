c5798c66bd9f9ee473d8ea006db7594c20f4eba2
debasishsahoo <debasish.sahoo96@gmail.com>
2021-02-17 18:39:23 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,7 @@
+// A function to display a message
+function sayHello() {
+    alert("Hello World!");
+}
+
+// Call function on click of the button
+document.getElementById("myBtn").onclick = sayHello;
