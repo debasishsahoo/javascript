0d91481cddd1d66180c1a9436d6f5a71deb1fac1
debasishsahoo96 <debasish.sahoo96@gmail.com>
2021-10-24 16:18:21 +0530

    Add Emmit

---
+++ 1. Javascript Variables/index.js
@@ -0,0 +1,27 @@
+//Variable shadowing
+/* let msg = 'Hello'
+function say() {
+    let msg = 'hi'
+    console.log('msg:', msg)
+}
+say()
+console.log('msg:', msg) */
+
+
+//Undefined
+/* var counter;
+console.log(counter);
+counter = 100; */
+
+//Using let and const keywords
+
+/* var a=22,b=20
+{
+    let temp=a;
+    a=b
+    b=temp
+}
+console.log(temp) */
+
+/* const pi= 3.14;
+pi = 3.141; */
